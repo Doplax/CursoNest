451abd54d5ebea7a4642a4dca318d9aa8c4059ec
Doplax <doplax@gmail.com>
2025-12-30 13:20:00 +0100

    Pipe - ParseUUIDPipe

--- 03-CarDealership/src/cars/cars.controller.ts
+++ 03-CarDealership/src/cars/cars.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, ParseUUIDPipe, Patch, Post } from '@nestjs/common';
 import { CarsService } from './cars.service';
+import { version } from 'os';
 
 @Controller('cars')
 export class CarsController {
@@ -14,7 +15,7 @@ export class CarsController {
   }
 
   @Get(':id')
-  getCarById( @Param('id') id: string ) {
+  getCarById( @Param('id',new ParseUUIDPipe({version:'4'})) id: string ) {
     return this.carsService.findOneById( id );
   }
 

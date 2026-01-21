ffba2ccb7127295eaab43a03a839de746b1b641b
Pol Valle <pol.valle@i3televison.es>
2026-01-21 12:14:01 +0100

    Pipes Globales - A nivel de Aplicación

--- 03-CarDealership/src/main.ts
+++ 03-CarDealership/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Elimina propiedades que no esten en el DTO
+      forbidNonWhitelisted: true, // Lanza un error si hay propiedades no definidas en el DTO
+    })
+  );
+  
   await app.listen(3000);
 }
 bootstrap();
